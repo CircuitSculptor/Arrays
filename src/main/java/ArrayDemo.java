3aacff6b6ddf5c7f41ca667f027107131eb673b6
Bartlomiej Drapinski <g00428194@atu.ie>
2024-10-16 14:58:34 +0100

    clean up array demo code

--- src/main/java/ArrayDemo.java
+++ src/main/java/ArrayDemo.java
@@ -25,17 +25,9 @@ public class ArrayDemo {
             System.out.println("Error. Cannot fill index 10 " + e.getMessage());
         }
 
-        System.out.println("Element at index 0: " + anArray[0]);
-        System.out.println("Element at index 1: " + anArray[1]);
-        System.out.println("Element at index 2: " + anArray[2]);
-        System.out.println("Element at index 3: " + anArray[3]);
-        System.out.println("Element at index 4: " + anArray[4]);
-        System.out.println("Element at index 5: " + anArray[5]);
-        System.out.println("Element at index 6: " + anArray[6]);
-        System.out.println("Element at index 7: " + anArray[7]);
-        System.out.println("Element at index 8: " + anArray[8]);
-        System.out.println("Element at index 9: " + anArray[9]);
-        System.out.println("Element at index 10: " + anArray[10]);
-
+        for  (int i = 0; i < anArray.length; i++)
+        {
+            System.out.println("Element at index " + i + ": " + anArray[i]);
+        }
     }
 }
